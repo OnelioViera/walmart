2990cd14c01ae0935edd9a86b1312686cce5b7ca
ojvwe <ojvwebdesign@gmail.com>
2023-07-12 07:19:55 -0400

    updated files

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import (
-    Flask,
-    render_template,
-    request,
-    session,
-    flash,
-    make_response,
-    redirect,
-)
-
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -23,20 +14,38 @@ def base():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # Perform login process
-        username = request.form["username"]
-        password = request.form["password"]
+        # Retrieve the email from the login form
+        email = request.form["email"]
 
-        # Validate username and password
-        if username in users and users[username] == password:
-            # Set Session variables or user tokens for authentication purposes
-            session["logged_in"] = True
-            session["username"] = username
-            flash("Login successful!")
-            return render_template(
-                "login.html",
-            )
-        else:
-            flash("Invalid username or password!")
+        # Store the email in the session
+        session["email"] = email
+
+        return redirect("/registration")
 
     return render_template("login.html")
+
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == "POST":
+        # Retrieve form data
+        email = request.form["email"]
+        first_name = request.form["firstName"]
+        last_name = request.form["lastName"]
+        password = request.form["newPassword"]
+
+        # Perform registration logic here (e.g., store user information in database)
+        # Replace this with your own implementation
+
+        # Render a success page or redirect to a different page
+        return render_template("success.html")
+
+    # Retrieve the email from the session
+    email = session.get("email")
+
+    # Render the registration form
+    return render_template("registration.html", email=email)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
